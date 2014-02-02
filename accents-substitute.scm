184f3f1aeac1b7301ca0613f13e13fd635a9d5a4
Mario Domenech Goulart <mario.goulart@gmail.com>
2014-02-02 08:59:12 -0200

    Install accents-substitute module
    That's a conveninece module which exports accents-substitute-latin1
    and accents-substitute-utf8 from their respective modules.
    
    As a intended side-effect, this change fixes a problem that would make
    chicken-install always resintall this egg when "accents-substitute" is
    in the dependencies list of some other egg.  Since accents-substitute
    didn't install an accents-substitute module (it only installed
    accents-substitute-latin1 and accents-substitute-utf8),
    chicken-install would not detect it is installed.  On the other hand,
    you can't use accents-substitute-latin1 or accents-substitute-utf8 in
    dependencies lists because they are not eggs (they are modules of
    eggs).

---
+++ accents-substitute.scm
@@ -0,0 +1,7 @@
+(module accents-substitute (accents-substitute-latin1 accents-substitute-utf8)
+
+(import chicken scheme)
+(use (rename accents-substitute-latin1 (accents-substitute accents-substitute-latin1))
+     (rename accents-substitute-utf8 (accents-substitute accents-substitute-utf8)))
+
+) ;; end module
